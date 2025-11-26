41dc591296e76f30b7c2ee81a4a3b2b6738e27f4
Geonx14 <jenerlaureano.bspi@gmail.com>
2025-11-26 14:52:33 +0800

    change connection

--- vmc_alumni/connection.php
+++ vmc_alumni/connection.php
@@ -3,5 +3,5 @@ date_default_timezone_set('Asia/Manila');
 
 //$conn = new mysqli('localhost','root','','iatms') or die("Could not connect to mysql".mysqli_error($con));
 
-$conn = new mysqli('srv1941.hstgr.io','u590251775_db_alumni_vmc','K=a:;6Dzq0','u590251775_db_alumni_vmc') or die("Could not connect to mysql".mysqli_error($con));
+$conn = new mysqli('localhost','u590251775_db_alumni_vmc','K=a:;6Dzq0','u590251775_db_alumni_vmc') or die("Could not connect to mysql".mysqli_error($con));
 ?>
