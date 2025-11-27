ebc3d862b35e5640cc1f0da7ce8df7b097578414
Geonx14 <jenerlaureano.bspi@gmail.com>
2025-11-27 08:38:07 +0800

    aa

--- vmc_alumni/query/participant_join.php
+++ vmc_alumni/query/participant_join.php
@@ -4,7 +4,7 @@ include '../connection.php';
 $user_id = $_POST['user_id'];
 $event_id = $_POST['event_id'];
 $status = $_POST['status'];
-
+$datetime = date('Y-m-d H:i:s');
 $is_exist = $conn->query("
     SELECT * FROM event_participants 
     WHERE event_id = '$event_id' 
@@ -15,7 +15,7 @@ if ($is_exist->num_rows > 0) {
 
     $conn->query("
         UPDATE event_participants 
-        SET status = '$status' 
+        SET status = '$status' ,data_participated='$datetime'
         WHERE event_id = '$event_id' 
           AND user_id = '$user_id'
     ");
@@ -25,8 +25,8 @@ if ($is_exist->num_rows > 0) {
 } else {
 
     $conn->query("
-        INSERT INTO event_participants (event_id, user_id, status) 
-        VALUES ('$event_id', '$user_id', '$status')
+        INSERT INTO event_participants (event_id, user_id, status,data_participated) 
+        VALUES ('$event_id', '$user_id', '$status','$datetime')
     ");
 
     echo "1";
